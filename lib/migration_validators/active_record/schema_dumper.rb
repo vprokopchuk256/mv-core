2dd813499c6b399179aec1759a158e22f47dae31
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2011-03-11 07:12:33 -0600

    Schema formatting fixed

--- lib/migration_validators/active_record/schema_dumper.rb
+++ lib/migration_validators/active_record/schema_dumper.rb
@@ -11,10 +11,9 @@ module MigrationValidators
         tables_without_validators(stream)
 
         stream.puts ""
-        stream.puts "#Validators"
-        stream.puts ""
+        stream.puts "  #Validators"
         MigrationValidators::Core::DbValidator.find(:all, :order => "table_name, column_name").each do |validator|
-          stream.puts "validate_column :#{validator.table_name}, :#{validator.column_name}, :#{validator.validator_name} => #{validator.options.inspect}"
+          stream.puts "  validate_column :#{validator.table_name}, :#{validator.column_name}, :#{validator.validator_name} => #{validator.options.inspect}"
         end
       end
 
