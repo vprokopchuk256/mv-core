aebd30912c0c40c4ea40b483770f806df072df64
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2014-12-05 16:08:26 +0200

    migration validator cleaned up

--- lib/mv/core/db/migration_validator.rb
+++ lib/mv/core/db/migration_validator.rb
@@ -7,16 +7,6 @@ module Mv
         validates :validator_name, presence: true
 
         serialize :options, Hash
-
-        scope :for_version, -> (version) { where(version: version) }
-
-        scope :recent, -> {
-          where("version = (SELECT MAX(version) FROM migration_validators)")
-        }
-
-        def dup_with_version(version)
-          return dup.tap { |v| v.version = version }
-        end
       end
     end
   end
