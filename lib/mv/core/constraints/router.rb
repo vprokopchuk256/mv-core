22f5ac5516b11a12021e5d1eff80fbf599223acd
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2014-12-08 14:52:40 +0200

    empty constraints router class added

---
+++ lib/mv/core/constraints/router.rb
@@ -0,0 +1,11 @@
+module Mv
+  module Core
+    module Constraints
+      class Router
+        def initialize()
+          
+        end
+      end
+    end
+  end
+end
---
+++ spec/mv/core/constraints/router_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+require 'mv/core/constraints/router'
+
+describe Mv::Core::Constraints::Router do
+  
+end
