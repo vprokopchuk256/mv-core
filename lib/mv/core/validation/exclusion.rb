2c2b6016d68491d71edcc6182b926d6d471e43e8
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2014-12-17 23:00:53 +0200

    exclusion validation stub

---
+++ lib/mv/core/validation/exclusion.rb
@@ -0,0 +1,23 @@
+module Mv
+  module Core
+    module Validation
+      class Exclusion
+        attr_reader :in, :message, :on, :create_trigger_name, :update_trigger_name, 
+                    :allow_nil, :allow_blank, :as
+
+        def initialize opts
+          opts.with_indifferent_access.tap do |opts|
+            @in = opts[:in]
+            @message = opts[:message]
+            @on = opts[:on]
+            @create_trigger_name = opts[:create_trigger_name]
+            @update_trigger_name = opts[:update_trigger_name]
+            @allow_nil = opts[:allow_nil]
+            @allow_blank = opts[:allow_blank]
+            @as = opts[:as]
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/mv/core/validation/exclusion_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+require 'mv/core/validation/exclusion'
+
+describe Mv::Core::Validation::Exclusion do
+  describe "#initialize" do
+    subject { described_class.new(in: :in, 
+                                  message: :message, 
+                                  on: :on, 
+                                  create_trigger_name: :create_trigger_name, 
+                                  update_trigger_name: :update_trigger_name, 
+                                  allow_nil: :allow_nil, 
+                                  allow_blank: :allow_blank, 
+                                  as: :as) }
+
+    its(:in) { is_expected.to eq(:in) }
+    its(:message) { is_expected.to eq(:message) }
+    its(:on) { is_expected.to eq(:on) }
+    its(:create_trigger_name) { is_expected.to eq(:create_trigger_name) }
+    its(:update_trigger_name) { is_expected.to eq(:update_trigger_name) }
+    its(:allow_nil) { is_expected.to eq(:allow_nil) }
+    its(:allow_blank) { is_expected.to eq(:allow_blank) }
+    its(:as) { is_expected.to eq(:as) }
+  end
+end
