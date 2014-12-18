300782eaad8ebd81f8e2c01d87245accd938044d
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2014-12-18 22:43:22 +0200

    presence validation validations

--- lib/mv/core/validation/presence.rb
+++ lib/mv/core/validation/presence.rb
@@ -2,23 +2,67 @@ module Mv
   module Core
     module Validation
       class Presence
+        include ActiveModel::Validations
+
         attr_reader :table_name, :column_name,
                     :message, :on, :create_trigger_name, :update_trigger_name, 
                     :allow_nil, :allow_blank, :as
+
+        validates :on, inclusion: { in: :available_on }
+        validates :allow_nil, :allow_blank, inclusion: { in: [true, false] }
+        validates :as, inclusion: { in: :available_as }
 
         def initialize(table_name, column_name, opts)
           @table_name = table_name
           @column_name = column_name
 
           opts.with_indifferent_access.tap do |opts|
-            @message = opts[:message]
-            @on = opts[:on]
-            @create_trigger_name = opts[:create_trigger_name]
-            @update_trigger_name = opts[:update_trigger_name]
-            @allow_nil = opts[:allow_nil]
-            @allow_blank = opts[:allow_blank]
-            @as = opts[:as]
+            @message = opts[:message] || default_message
+            @on = opts[:on] || default_on
+            @create_trigger_name = opts[:create_trigger_name] || default_create_trigger_name
+            @update_trigger_name = opts[:update_trigger_name] || default_update_trigger_name
+            @allow_nil = opts[:allow_nil] || default_allow_nil
+            @allow_blank = opts[:allow_blank] || default_allow_blank
+            @as = opts[:as] || default_as
           end
+        end
+
+        protected
+
+        def available_as
+          [:trigger, :check]
+        end
+
+        def available_on 
+          [:save, :update, :create]
+        end
+
+        def default_message
+          "Format violated on the table #{table_name} column #{column_name}"
+        end
+
+        def default_on
+          :save
+        end
+
+        def default_as
+          :trigger
+        end
+
+        def default_create_trigger_name
+          [:save, :create].include?(on.to_sym) ? "trg_mv_#{table_name}_ins" : nil
+        end
+
+        def default_update_trigger_name
+          [:save, :update].include?(on.to_sym) ? "trg_mv_#{table_name}_upd" : nil
+        end
+
+        def default_allow_nil
+          false
+        end
+
+        def default_allow_blank
+          false
         end
       end
     end
--- spec/mv/core/validation/presence_spec.rb
+++ spec/mv/core/validation/presence_spec.rb
@@ -3,24 +3,168 @@ require 'spec_helper'
 require 'mv/core/validation/presence'
 
 describe Mv::Core::Validation::Presence do
-  describe "#initialize" do
-    subject { described_class.new(:table_name, :column_name,
-                                  message: :message, 
-                                  on: :on, 
-                                  create_trigger_name: :create_trigger_name, 
-                                  update_trigger_name: :update_trigger_name, 
-                                  allow_nil: :allow_nil, 
-                                  allow_blank: :allow_blank, 
-                                  as: :as) }
+  def instance(opts = {})
+    described_class.new(:table_name, :column_name,
+                        { message: :message, 
+                          on: :create, 
+                          create_trigger_name: :create_trigger_name, 
+                          update_trigger_name: :update_trigger_name, 
+                          allow_nil: true,
+                          allow_blank: true, 
+                          as: :check}.merge(opts))
+  end
 
+  subject { instance }
+
+  describe "#initialize" do
     its(:table_name) { is_expected.to eq(:table_name) }
     its(:column_name) { is_expected.to eq(:column_name) }
     its(:message) { is_expected.to eq(:message) }
-    its(:on) { is_expected.to eq(:on) }
+    its(:on) { is_expected.to eq(:create) }
     its(:create_trigger_name) { is_expected.to eq(:create_trigger_name) }
     its(:update_trigger_name) { is_expected.to eq(:update_trigger_name) }
-    its(:allow_nil) { is_expected.to eq(:allow_nil) }
-    its(:allow_blank) { is_expected.to eq(:allow_blank) }
-    its(:as) { is_expected.to eq(:as) }
+    its(:allow_nil) { is_expected.to be_truthy }
+    its(:allow_blank) { is_expected.to be_truthy }
+    its(:as) { is_expected.to eq(:check) }
+  end
+
+  describe "default values" do
+    describe ":allow_nil" do
+      subject { instance(allow_nil: nil) }
+      
+      its(:allow_nil) { is_expected.to be_falsey }
+    end
+
+    describe ":allow_blank" do
+      subject { instance(allow_blank: nil) }
+      
+      its(:allow_blank) { is_expected.to be_falsey }
+    end
+
+    describe ":message" do
+      subject { instance(message: nil) }
+      
+      its(:message) { is_expected.to eq('Format violated on the table table_name column column_name') }
+    end
+
+    describe ":on" do
+      subject { instance(on: nil) } 
+
+      its(:on) { is_expected.to eq(:save) }
+    end
+
+    describe ":as" do
+      subject { instance(as: nil) } 
+
+      its(:as) { is_expected.to eq(:trigger) }
+    end
+
+    describe ":create_trigger_name" do
+      describe "when :on == :save" do
+        subject { instance(on: :save, create_trigger_name: nil) }
+
+        its(:create_trigger_name) { is_expected.to eq('trg_mv_table_name_ins') }
+      end
+
+      describe "when :on == :create" do
+        subject { instance(on: :create, create_trigger_name: nil) }
+        
+        its(:create_trigger_name) { is_expected.to eq('trg_mv_table_name_ins') }
+      end
+
+      describe "when :on == :update" do
+        subject { instance(on: :update, create_trigger_name: nil) }
+        
+        its(:create_trigger_name) { is_expected.to be_nil }
+      end
+    end
+
+    describe ":update_trigger_name" do
+      describe "when :on == :save" do
+        subject { instance(on: :save, update_trigger_name: nil) }
+
+        its(:update_trigger_name) { is_expected.to eq('trg_mv_table_name_upd') }
+      end
+
+      describe "when :on == :create" do
+        subject { instance(on: :create, update_trigger_name: nil) }
+        
+        its(:update_trigger_name) { is_expected.to be_nil }
+      end
+
+      describe "when :on == :update" do
+        subject { instance(on: :update, update_trigger_name: nil) }
+        
+        its(:update_trigger_name) { is_expected.to eq('trg_mv_table_name_upd') }
+      end
+    end
+  end
+
+  describe "validation" do
+    it { is_expected.to be_valid }
+
+    describe ":on" do
+      [:save, :update, :create].each do |event|
+        describe "when :on == #{event}" do
+          subject { instance(on: event) }
+         
+          it { is_expected.to be_valid }
+        end
+      end
+
+      describe "when :on == :invalid_event" do
+        subject { instance(on: :invalid_event) }
+       
+        it { is_expected.to be_invalid }
+      end
+    end
+
+    describe ":allow_nil" do
+      [true, false].each do |value|
+        describe "when :allow_nil == #{value}" do
+          subject { instance(allow_nil: value) }
+         
+          it { is_expected.to be_valid }
+        end
+      end
+      
+      describe "when :allow_nil == :non_boolean_value" do
+        subject { instance(allow_nil: :non_boolean_value) }
+       
+        it { is_expected.to be_invalid }
+      end
+    end
+
+    describe ":allow_blank" do
+      [true, false].each do |value|
+        describe "when :allow_blank == #{value}" do
+          subject { instance(allow_blank: value) }
+         
+          it { is_expected.to be_valid }
+        end
+      end
+      
+      describe "when :allow_blank == :non_boolean_value" do
+        subject { instance(allow_blank: :non_boolean_value) }
+       
+        it { is_expected.to be_invalid }
+      end
+    end
+
+    describe ":as" do
+      [:trigger, :check].each do |constraint_type|
+        describe "when :as == #{constraint_type}" do
+          subject { instance(as: constraint_type) }
+         
+          it { is_expected.to be_valid }
+        end
+      end
+
+      describe "when :as == :invalid_constraint_type" do
+        subject { instance(as: :invalid_constraint_type) }
+       
+        it { is_expected.to be_invalid }
+      end
+    end
   end
 end
