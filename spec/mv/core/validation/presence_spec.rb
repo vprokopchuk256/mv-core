23a6db5b7e2c7f6b05155ec4e8423817d3d34ecf
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2014-12-19 19:06:14 +0200

    presence validation commons are moved to base

--- spec/mv/core/validation/presence_spec.rb
+++ spec/mv/core/validation/presence_spec.rb
@@ -44,7 +44,7 @@ describe Mv::Core::Validation::Presence do
     describe ":message" do
       subject { instance(message: nil) }
       
-      its(:message) { is_expected.to eq('Format violated on the table table_name column column_name') }
+      its(:message) { is_expected.to eq('Presence violated on the table table_name column column_name') }
     end
 
     describe ":on" do
