23aeecad28365d09b306a221dfc9b45f175a4874
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2014-12-25 10:36:27 +0200

    synchronize constraints stub

---
+++ lib/mv/core/services/synchronize_constraints.rb
@@ -0,0 +1,11 @@
+module Mv
+  module Core
+    module Services
+      class SynchronizeConstraints
+        def initialize(deletions, updates, additions)
+          
+        end
+      end
+    end
+  end
+end
---
+++ spec/mv/core/services/synchronize_constraints_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+require 'mv/core/services/synchronize_constraints'
+
+describe Mv::Core::Services::SynchronizeConstraints do
+end
