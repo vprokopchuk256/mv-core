103e1f4e95b77985b3a807f7907e91d52d7b7475
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-03 21:22:49 +0200

    index constraint full implementation

--- spec/mv/core/constraint/index_spec.rb
+++ spec/mv/core/constraint/index_spec.rb
@@ -7,6 +7,7 @@ require 'mv/core/error'
 describe Mv::Core::Constraint::Index do
   before do
     Mv::Core::Services::CreateMigrationValidatorsTable.new.execute
+    Mv::Core::Db::MigrationValidator.delete_all
   end
 
   let(:index_description) { Mv::Core::Constraint::Description.new(:idx_mv_table_name, :index) }
@@ -17,11 +18,6 @@ describe Mv::Core::Constraint::Index do
     its(:options) { is_expected.to eq({}) }
     its(:name) { is_expected.to eq(:idx_mv_table_name) }
     its(:validations) { is_expected.to eq([]) }
-  end
-
-  describe "#constraint interface" do
-    it { is_expected.to respond_to(:create) }
-    it { is_expected.to respond_to(:delete) }
   end
 
   describe "#<=>" do
@@ -77,6 +73,98 @@ describe Mv::Core::Constraint::Index do
       end
       
       it { is_expected.to eq(other_index) }
+    end
+  end
+
+  describe "SQL methods" do
+    before do
+      if ActiveRecord::Base.connection.index_name_exists?(:table_name, :idx_mv_table_name, false)
+        ActiveRecord::Base.connection.remove_index!(:table_name, :idx_mv_table_name)
+      end
+
+      Mv::Core::Migration::Base.with_suppressed_validations do
+        ActiveRecord::Base.connection.drop_table(:table_name) if ActiveRecord::Base.connection.table_exists?(:table_name)
+
+        ActiveRecord::Base.connection.create_table(:table_name) do |t|
+          t.string :column_name
+          t.string :column_name_1
+        end
+      end
+    end
+
+    let(:uniqueness) { 
+      Mv::Core::Validation::Uniqueness.new(:table_name, 
+                                           :column_name, 
+                                           as: :index, 
+                                           index_name: :idx_mv_table_name) 
+    }
+
+    before do
+      index.validations << uniqueness
+    end
+
+    describe "#create" do
+      before { index.create }
+
+      subject { ActiveRecord::Base.connection.indexes(:table_name).find{|idx| idx.name == "idx_mv_table_name"} }
+
+      it { is_expected.to be_present }
+      its(:name) { is_expected.to eq('idx_mv_table_name')}
+      its(:columns) { is_expected.to eq(['column_name'])}
+
+      describe 'when called second time' do
+        before do
+          index.validations << Mv::Core::Validation::Uniqueness.new(:table_name, 
+                                                                               :column_name_1, 
+                                                                               as: :index, 
+                                                                               index_name: :idx_mv_table_name) 
+          index.create
+        end
+
+        its(:columns) { is_expected.to eq(['column_name', 'column_name_1'])}
+      end
+    end
+
+    describe "#update" do
+      subject { ActiveRecord::Base.connection.indexes(:table_name).find{|idx| idx.name == "idx_mv_table_name"} }
+
+      describe "when index exists" do
+        before do 
+          index.create 
+          index.validations << Mv::Core::Validation::Uniqueness.new(:table_name, 
+                                                                               :column_name_1, 
+                                                                               as: :index, 
+                                                                               index_name: :idx_mv_table_name) 
+          index.update(index)
+        end
+
+        its(:columns) { is_expected.to eq(['column_name', 'column_name_1'])}
+      end
+
+      describe "when index does not exist" do
+        before do
+          index.update(index)
+        end
+        
+        it { is_expected.to be_present }
+      end
+    end
+
+    describe "#delete" do
+      before { 
+        ActiveRecord::Base.connection.add_index(:table_name, :column_name, name: :idx_mv_table_name) 
+        index.delete
+      }
+
+      subject { ActiveRecord::Base.connection.indexes(:table_name).find{|idx| idx.name == "idx_mv_table_name"} }
+
+      it { is_expected.to be_nil }
+
+      describe "when called second time" do
+        it "should not raise error"  do
+          expect{ index.delete }.not_to raise_error
+        end
+      end
     end
   end
 end
