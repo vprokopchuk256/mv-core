f6e00598c753a067f81cecc3f9f81f6478610f9f
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-05 17:43:37 +0200

    validation builder factory

---
+++ lib/mv/core/validation/builder/factory.rb
@@ -0,0 +1,35 @@
+require 'mv/core/validation/builder/exclusion'
+require 'mv/core/validation/builder/inclusion'
+require 'mv/core/validation/builder/length'
+require 'mv/core/validation/builder/presence'
+require 'mv/core/validation/builder/uniqueness'
+
+module Mv
+  module Core
+    module Validation
+      module Builder
+        class Factory
+          def create_builder validation
+            factory_map[validation.class].new(validation)
+          end
+
+          def define_builder validation_class, builder_class
+            factory_map[validation_class] = builder_class
+          end
+
+          private
+
+          def factory_map
+            @factory_map ||= {
+              Mv::Core::Validation::Exclusion => Mv::Core::Validation::Builder::Exclusion,
+              Mv::Core::Validation::Inclusion => Mv::Core::Validation::Builder::Inclusion,
+              Mv::Core::Validation::Length => Mv::Core::Validation::Builder::Length,
+              Mv::Core::Validation::Presence => Mv::Core::Validation::Builder::Presence,
+              Mv::Core::Validation::Uniqueness => Mv::Core::Validation::Builder::Uniqueness
+            }
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/mv/core/validation/builder/factory_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+require 'mv/core/validation/builder/factory'
+
+describe Mv::Core::Validation::Builder::Factory do
+  subject(:factory) { described_class.new }
+
+  describe "#create_builder" do
+    subject { factory.create_builder(validation) }
+
+    describe "exclusion" do
+      let(:validation) { Mv::Core::Validation::Exclusion.new(:table_name, :column_name, in: [1, 2]) }
+
+      it { is_expected.to be_an_instance_of(Mv::Core::Validation::Builder::Exclusion) }
+    end
+   
+    describe "inclusion" do
+      let(:validation) { Mv::Core::Validation::Inclusion.new(:table_name, :column_name, in: [1, 2]) }
+
+      it { is_expected.to be_an_instance_of(Mv::Core::Validation::Builder::Inclusion) }
+    end 
+
+    describe "length" do
+      let(:validation) { Mv::Core::Validation::Length.new(:table_name, :column_name, in: [1, 2]) }
+
+      it { is_expected.to be_an_instance_of(Mv::Core::Validation::Builder::Length) }
+    end
+
+    describe "presence" do
+      let(:validation) { Mv::Core::Validation::Presence.new(:table_name, :column_name, in: [1, 2]) }
+
+      it { is_expected.to be_an_instance_of(Mv::Core::Validation::Builder::Presence) }
+    end
+
+    describe "uniqueness" do
+      let(:validation) { Mv::Core::Validation::Uniqueness.new(:table_name, :column_name, in: [1, 2]) }
+
+      it { is_expected.to be_an_instance_of(Mv::Core::Validation::Builder::Uniqueness) }
+    end
+  end
+
+  describe "#define_builder" do
+    let(:klass) { CustomValidationBuilderClass = Class.new(Mv::Core::Validation::Builder::Exclusion) }
+    let(:validation) { Mv::Core::Validation::Exclusion.new(:table_name, :column_name, in: [1, 2]) }
+
+    before { factory.define_builder(Mv::Core::Validation::Exclusion, klass) }
+
+    subject { factory.create_builder(validation) }
+
+    it { is_expected.to be_an_instance_of(klass) }
+
+    after { factory.define_builder(Mv::Core::Validation::Exclusion, Mv::Core::Validation::Builder::Exclusion) }
+  end
+end
