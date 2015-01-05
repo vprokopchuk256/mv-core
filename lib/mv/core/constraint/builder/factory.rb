7212aa7b7bb7a694b51764b095de7802c5205be1
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-05 22:14:58 +0200

    constraint builder factory

---
+++ lib/mv/core/constraint/builder/factory.rb
@@ -0,0 +1,36 @@
+require 'mv/core/constraint/builder/index'
+require 'mv/core/constraint/builder/trigger'
+
+module Mv
+  module Core
+    module Constraint
+      module Builder
+        class Factory
+          include Singleton
+
+          def create_builder constraint
+            factory_map[constraint.class].new(constraint)
+          end
+
+          def define_builder constraint_class, builder_class
+            factory_map[constraint_class] = builder_class
+          end
+
+          class << self
+            delegate :create_builder, :define_builder, to: :instance
+          end
+
+
+          private
+
+          def factory_map
+            @factory_map ||= {
+              Mv::Core::Constraint::Index => Mv::Core::Constraint::Builder::Index, 
+              Mv::Core::Constraint::Trigger => Mv::Core::Constraint::Builder::Trigger
+            }
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/mv/core/constraint/builder/factory_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+require 'mv/core/constraint/builder/factory'
+
+describe Mv::Core::Constraint::Builder::Factory do
+  describe "#create_builder" do
+    subject { described_class.create_builder(constraint) }
+
+    describe "for index constraint" do
+      let(:index_description) { Mv::Core::Constraint::Description.new(:idx_table_name, :index) }
+      let(:constraint) { Mv::Core::Constraint::Index.new(index_description)}
+
+      it { is_expected.to be_a_kind_of(Mv::Core::Constraint::Builder::Index) }
+    end
+
+    describe "for trigger constraint" do
+      let(:trigger_description) { Mv::Core::Constraint::Description.new(:trg_table_name_upd, :trigger, event: :update) }
+      let(:constraint) { Mv::Core::Constraint::Trigger.new(trigger_description)}
+      
+      it { is_expected.to be_a_kind_of(Mv::Core::Constraint::Builder::Trigger) }
+    end
+
+    describe "when custom constraint builder defined" do
+      let(:trigger_description) { Mv::Core::Constraint::Description.new(:trg_table_name_upd, :trigger, event: :update) }
+      let(:klass) { Class.new(Mv::Core::Constraint::Builder::Trigger) }
+      let(:constraint) { Mv::Core::Constraint::Trigger.new(trigger_description)}
+
+      before { described_class.define_builder(Mv::Core::Constraint::Trigger, klass) }
+
+      it { is_expected.to be_a_kind_of(klass) }
+
+      after { described_class.define_builder(Mv::Core::Constraint::Trigger, Mv::Core::Constraint::Builder::Trigger) }
+    end
+  end
+end
