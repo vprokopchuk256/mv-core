282febfb5e28a07d36e1d53c4b59861601a911e8
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-11 20:43:38 +0200

    outdated code removed

--- lib/options.rb
+++
@@ -1,7 +0,0 @@
-# module MigrationValidators
-#   @migration_validators_table_name = :migration_validators
-
-#   class << self
-#     attr_accessor :migration_validators_table_name
-#   end
-# end
--- spec/migration_validators/active_record/connection_adapters/table_definition_spec.rb
+++
@@ -1,4 +0,0 @@
-# require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-
-# describe ActiveRecord::ConnectionAdapters::TableDefinition, "migration validators extension", :type => :mv_test do
-# end
