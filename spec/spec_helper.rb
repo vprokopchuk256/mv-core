9bf3c61c98150489ad4daac8e890003263c51982
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-13 19:45:01 +0200

    spec_helper cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,8 @@
-# $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-# $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'rspec/its'
 require 'mv-core'
 require 'shoulda'
 require 'factory_girl'
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-# Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 ActiveRecord::Migration.verbose = false
 
