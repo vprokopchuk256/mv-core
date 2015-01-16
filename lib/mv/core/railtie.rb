7994c5be05e904e740d90637ac7a3e837d5d1051
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-16 16:13:25 +0200

    rake tasks

--- lib/mv/core/railtie.rb
+++ lib/mv/core/railtie.rb
@@ -1,11 +1,25 @@
-require 'mv/core/services/create_migration_validators_table.rb'
+require 'mv/core/services/uninstall'
+require 'mv/core/services/delete_constraints'
+require 'mv/core/services/create_constraints'
 
 module Mv
   module Core
     class Railtie < ::Rails::Railtie
-      initializer 'mv-core.initilization.migration_validators_table', after: 'active_record.initialize_database' do
-        # Mv::Core::Services::CreateMigrationValidatorsTable.new.execute
-      end
+      rake_tasks do
+        namespace :mv do
+          task :uninstall => :environment  do
+            Mv::Core::Services::Uninstall.new.execute
+          end
+
+          task :delete_constraints, [:tables] => :environment do |task, args|
+            Mv::Core::Services::DeleteConstraints.new((args[:tables] || '').split(/\s+/)).execute
+          end
+
+          task :create_constraints, [:tables] => :environment do |task, args|
+            Mv::Core::Services::CreateConstraints.new((args[:tables] || '').split(/\s+/)).execute
+          end
+        end
+      end 
     end
   end
 end
