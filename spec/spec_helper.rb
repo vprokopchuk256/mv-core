d2d2f082529f8555c23088c2e398cb2c49763de7
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-19 15:44:43 +0200

    rspec fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
+require 'mv-core'
 require 'rspec'
 require 'rspec/its'
 require 'shoulda'
 require 'factory_girl'
 
-ActiveRecord::Migration.verbose = false
+::ActiveRecord::Migration.verbose = false
 
 require 'coveralls'
 Coveralls.wear!
@@ -14,7 +15,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   
 	config.before :each do
-    ActiveRecord::Base.remove_connection if ::ActiveRecord::Base.connected?
-    ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+    ::ActiveRecord::Base.remove_connection if ::ActiveRecord::Base.connected?
+    ::ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
   end	  
 end
